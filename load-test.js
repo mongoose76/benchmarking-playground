4aeb2be6957de51788c17cace999ecd1be7f1b81
Doru Paraschiv <doru.paraschiv@vgw.co>
2020-06-27 00:22:59 +0300

    test: updated k6 load testing script

--- load-test.js
+++ load-test.js
@@ -1,12 +1,25 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 export let options = {
   vus: 1000,
   duration: '30s',
+  thresholds: {
+    'http_req_duration': ['p(99)<100'], // 99% of requests must complete below 1.5s
+  }
 };
 
 export default function() {
-  http.get('http://127.0.0.1:3000/');
-  sleep(1);
+  let event = { 
+    type: "spin",
+    value: Math.round(100 * Math.random())
+  };
+
+  let res = http.post('http://127.0.0.1:3000/events', event);
+
+  check(res, {
+    'is status 201': (r) => r.status === 201,
+  });
+
+  sleep(500);
 }
