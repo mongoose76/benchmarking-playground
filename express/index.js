3f6efe9e0873358d1eef58dd49ec48b300df969a
Doru Paraschiv <doru.paraschiv@vgw.co>
2020-07-05 16:59:17 +0200

    refactor: use Client instead of Pool directly

--- express/index.js
+++ express/index.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+const { Pool, Client } = require('pg');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -7,39 +7,73 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 })
 
-async function query() {
-  const q = `
-  INSERT INTO 
-  public."Events"
-  (
-    type,
-    value,
-    "createdAt",
-    "updatedAt"
-  )
-  VALUES (
-    'spin',
-    2,
-    now(),
-    now()
-  )`
+async function runQuery(q) {
+  const client = await pool.connect();
+  let res = {};
+
   try {
-    return pool.query(q);
+    res = await client.query(q);
   } catch (err) {
     console.log(err);
+  } finally {
+    client.release();
   }
   
-  return {};
+  return res;
 };
+
+async function createTable() {
+  const q = `CREATE TABLE IF NOT EXISTS public."Events" (
+    id SERIAL,
+    type VARCHAR(255),
+    value INTEGER,
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL,
+    "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
+    CONSTRAINT "Events_pkey" PRIMARY KEY(id)
+  )
+  WITH (oids = false);
+  `;
+  await runQuery(q);
+}
+async function deleteEvents() {
+  const q = `
+    TRUNCATE 
+    public."Events"
+  `;
+  await runQuery(q);
+};
+
+async function insertEvent() {
+  const q = `
+    INSERT INTO 
+    public."Events"
+    (
+      type,
+      value,
+      "createdAt",
+      "updatedAt"
+    )
+    VALUES (
+      'spin',
+      2,
+      now(),
+      now()
+    ) RETURNING "id","type","value","createdAt","updatedAt"
+  `;
+
+  return await runQuery(q);
+};
+
+createTable();
+deleteEvents();
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', async (req, res) => {
-  const { id } = req.params;
-  const { command } = await query();
-  res.send(command);
+  let r = await insertEvent();
+  res.send({});
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
