92c6a3321ceb420b33a5beb23cf39f34800e66de
Doru Paraschiv <doru.paraschiv@vgw.co>
2020-07-05 17:25:17 +0200

    chore: small test updates

--- express/scripts/load-test-get.js
+++ express/scripts/load-test-get.js
@@ -1,11 +1,11 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-let vus = 3900;
+let vus = 2000;
 export let options = {
   stages: [
-    { duration: "30s", target: vus }, // simulate ramp-up of traffic from 1 to max users over 30 s.
-    { duration: "30s", target: vus }, // stay at max users for 30 s
+    { duration: "5s", target: vus }, // simulate ramp-up of traffic from 1 to max users over 30 s.
+    { duration: "55s", target: vus }, // stay at max users for 30 s
   ],
   thresholds: {
     'http_req_duration': ['p(99)<100'], // 99% of requests must complete below 100ms
