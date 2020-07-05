edd3e20b2ea7e15f7e3fb0a3be2c7ffc3e7ce20a
Doru Paraschiv <doru.paraschiv@vgw.co>
2020-07-05 18:15:35 +0200

    refactor: reading vus from environment

--- fastify/scripts/load-test-get.js
+++ fastify/scripts/load-test-get.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-let vus = 2000;
+let vus = __ENV.VUS;
 export let options = {
   stages: [
     { duration: "5s", target: vus }, // simulate ramp-up of traffic from 1 to max users over 30 s.
@@ -13,7 +13,7 @@ export let options = {
 };
 
 export default function() {
-  let url = 'http://127.0.0.1:3000/';
+  let url = 'http://127.0.0.1:3000';
   let res = http.get(url);
 
   check(res, {
