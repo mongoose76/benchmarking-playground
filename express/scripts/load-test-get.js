440d6875bbb22dc01988fee7a4c0129d50d7778d
Doru Paraschiv <doru.paraschiv@vgw.co>
2020-07-05 18:05:39 +0200

    refactor: reading vus from environment

--- express/scripts/load-test-get.js
+++ express/scripts/load-test-get.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
-let vus = 2000;
+let vus = __ENV.VUS;
 export let options = {
   stages: [
     { duration: "5s", target: vus }, // simulate ramp-up of traffic from 1 to max users over 30 s.
