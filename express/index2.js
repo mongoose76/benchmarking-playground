bcef36d2d2aa2e03740e8714c9ea06c0a6a2481f
Doru Paraschiv <doru.paraschiv@vgw.co>
2020-07-05 12:39:53 +0200

    feat: added sequelize version of the test

---
+++ express/index2.js
@@ -0,0 +1,46 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL, { logging: false });
+
+class Event extends Model {}
+
+Event.init({
+  // Model attributes are defined here
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  value: {
+    type: DataTypes.INTEGER
+    // allowNull defaults to true
+  }
+}, {
+  // Other model options go here
+  sequelize, // We need to pass the connection instance
+  modelName: 'Event' // We need to choose the model name
+});
+
+// the defined model is the class itself
+console.log(Event === sequelize.models.Event); // true
+
+try {
+  sequelize.sync({force: true});
+} catch (err) {
+  console.log(err);
+}
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+  let ev = Event.build({ type: 'spin', value: 2 });
+  let x = ev.save();
+  await x;
+  res.send({});
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
