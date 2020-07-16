a046fb66921d7e027c670354b4c71bad4aa98149
mongoose76 <doru.paraschiv@gmail.com>
2020-07-17 00:04:13 +0300

    refactor: increase cursor CHUNK_SIZE and order by id (indexed)

--- event-sourcing-cqrs/db/index.js
+++ event-sourcing-cqrs/db/index.js
@@ -41,16 +41,17 @@ module.exports = function(app) {
   async function loadEvents(processFn) {
     app.log.debug("Loading events from DB");
     const connection = await pool.connect();
-    const CHUNK_SIZE = 100000;
+    const CHUNK_SIZE = 500000;
     try {
-      const text = 'SELECT * FROM "Events" ORDER BY "createdAt" ASC';
+      const text = 'SELECT * FROM "Events" ORDER BY "id" ASC';
       const cursor = connection.query(new Cursor(text));
-
+      let idx = 0;
       do {
         const rows = await readCursor(cursor, CHUNK_SIZE);
         if (rows.length === 0) {
             break;
         }
+        app.log.info(`Loading events ${idx * CHUNK_SIZE} to ${++idx * CHUNK_SIZE}`);
         rows.forEach(row => {
           let dbEv = row.payload;
           dbEv.id = row.id;
